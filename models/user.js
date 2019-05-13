d0e801e0a76b18c3c898bcf333c2b9b4c93929d5
norahx3x <alnoor0416@gmail.com>
2019-05-13 14:38:20 +0300

    backend

--- models/user.js
+++ models/user.js
@@ -1,13 +1,49 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
-const userSchema = new mongoose.Schema({
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
     email : {type: String, required : true, unique: true},
+    parent: {type: Boolean, required : true},
     name:  {type: String, required: true },
-    username:  {type: String, required : true, unique: true},
-    password: {type: String, required: true }
+    userName:  {type: String, required : true, unique: true},
+    password: {type: String, required: true },
+    parentUser:{ type: Schema.Types.ObjectId, ref : 'User'},
+    kids:[{ type: Schema.Types.ObjectId, ref : 'User'}]
 },{timestamps: true})
 
+
+userSchema.pre('save',function(next){
+    let user = this
+
+    if(user.password && user.isModified('password')){
+        
+      bcrypt.hash(user.password, saltRounds, (err, hash)=>{
+        if(err){ return next()}
+
+        user.password = hash
+        next()
+      })
+    }
+
+})
+userSchema.methods.verifyPassword = (plainPassword, hashedPassword, cb) => {
+
+    bcrypt.compare(plainPassword, hashedPassword, (err, response) => {
+      if(err) { 
+        return cb(err) 
+      }
+      return cb(null, response)
+    })
+   }
 
 const User = mongoose.model('User', userSchema)
 module.exports.userSchema = userSchema
 module.exports.User = User;
+
+
+
+
+
--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const app = express();
 require('dotenv').config();
 //get port 
 const port = process.env.PORT
+const session = require('express-session')
+//jwt and passports
+const jwt = require('jsonwebtoken')
+const passport = require('passport')
+
 const ejs = require('ejs');
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 // get routs 
 const bookRouts = require('./routes/books');
+// const authRouts = require('./routes/auth.routes');
 
 mongoose.connect('mongodb://localhost/', {useNewUrlParser : false})
 .then(()=> console.log('Mongodb is running'),(err)=> console.log(err) )
@@ -17,16 +23,32 @@ app.set('view engine', 'ejs');
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', true);
 
+
 //true for embaded ojects in schema/db 
 app.use(express.urlencoded({extended:true}));
 //post man instad of ejs
 app.use(express.json())
-
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
+
+//create session for passport
+app.use(session({
+  secret : "test",
+  resave : false,
+  saveUninitialized : true
+ }))
+
+ app.use(passport.initialize())
+ app.use(passport.session())
+ 
 //use routs 
 app.use('/books', bookRouts);
+app.use('/api/auth', require('./routes/auth.routes'))
 
+//cannot find route
+app.use('*', (request, response) => {
+  response.status(404).json({message : "Data not found!"})
+ })
 
 app.get('/', (req, res)=>{
     res.send('Hello world');
@@ -35,6 +57,7 @@ app.get('/', (req, res)=>{
   app.get('/somedata', (req, res) => {
     res.send('here is your information');
 });
+
   // listener 
 
 app.listen(port, ()=> {
