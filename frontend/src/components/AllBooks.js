00ead64c043fe7bbd27bbf1c1e4c7861480efe21
AceelAlArifi <aceelalarifi@gmail.com>
2019-05-13 13:15:54 +0300

    Components Base READY

---
+++ frontend/src/components/AllBooks.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class AllBooks extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Discover All Books</h1>
+           
+            </div>
+        )
+    }
+}
+
+
+
+export default AllBooks
---
+++ frontend/src/components/CreateEvent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class CreateEvent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Create Event</h1>
+
+            </div>
+        )
+    }
+}
+
+
+
+export default CreateEvent
