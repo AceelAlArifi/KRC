a7d95d86fd02cb45a530ae4e21e149dbeae4ac7d
norahx3x <alnoor0416@gmail.com>
2019-05-15 16:20:01 +0300

    register dialog

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -8,7 +8,6 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import SweetAlert from 'sweetalert-react';
 
 function validateLength(element) {
     // true means invalid, so our conditions got reversed
