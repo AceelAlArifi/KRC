d40c38b9bb06b8b943120d05c061ab73eac40edc
AceelAlArifi <aceelalarifi@gmail.com>
2019-05-16 16:47:45 +0300

    POSTS

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -12,19 +12,21 @@ class Posts extends Component {
                 <div>
                     <h1 className="welcome">Discover new intresting stuff</h1>
                 </div>
-
+<div className="container">
                 <Card className="card">
-                <Card.Title>{this.props.book.title}</Card.Title>
+                    <Card.Title>Wow in the World</Card.Title>
                     <Card.Body className="card">
                         <Card.Text>
-                            By: {this.props.book.author}
+                           Hosts Mindy Thomas and Guy Raz guide curious kids and their grown-ups on a journey into the wonders of the world around them. 
+                           We'll go inside our brains, out into space and deep into the coolest new stories in science and technology.
                         </Card.Text>
                         <Card.Text>
-                            Year of publish:{this.props.book.year}
+                            From NPR
                         </Card.Text>
-                        <a   href= {`/Book/${this.props.book._id}`}  className="roll-button button-slider">Discover more!</a>
+                        <a   href="#" className="roll-button button-slider">Discover more!</a>
                     </Card.Body>
                 </Card>
+                </div>
             </div>
         )
     }
